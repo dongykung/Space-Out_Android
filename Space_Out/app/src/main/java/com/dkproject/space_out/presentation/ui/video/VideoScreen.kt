095a4c23f654d75dc05329a7468ef699135079b0
kim dong kyung <kdkd422@naver.com>
2025-03-28 17:11:13 +0900

    [feat] 비디오 진행 중에도 소리 수정할 수 있는 다이얼로그 작성

--- Space_Out/app/src/main/java/com/dkproject/space_out/presentation/ui/video/VideoScreen.kt
+++ Space_Out/app/src/main/java/com/dkproject/space_out/presentation/ui/video/VideoScreen.kt
@@ -11,6 +11,7 @@ import androidx.compose.animation.fadeIn
 import androidx.compose.animation.fadeOut
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
@@ -19,10 +20,14 @@ import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.systemBarsPadding
+import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.filled.ArrowBack
+import androidx.compose.material.icons.filled.Close
 import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
@@ -41,6 +46,7 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalConfiguration
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
@@ -56,6 +62,7 @@ import androidx.media3.ui.PlayerView
 import com.dkproject.space_out.R
 import com.dkproject.space_out.presentation.Component.SelectVideoBottomSheet
 import com.dkproject.space_out.presentation.ui.sound.SoundCompactListView
+import com.dkproject.space_out.presentation.ui.sound.SoundItem
 import com.dkproject.space_out.presentation.ui.sound.SoundUiState
 import kotlinx.coroutines.delay
 
@@ -66,7 +73,6 @@ fun VideoScreen(
     videoUri: String,
     viewModel: VideoViewModel = hiltViewModel(),
     soundUiState: SoundUiState,
-    windowWidthSizeClass: WindowWidthSizeClass,
     updateVolume: (String, Int) -> Unit,
     onBackClick: () -> Unit
 ) {
@@ -76,6 +82,10 @@ fun VideoScreen(
     val activity = (context as? Activity)
     var lifecycle by remember { mutableStateOf(Lifecycle.Event.ON_START) }
     val lifecycleOwner = androidx.lifecycle.compose.LocalLifecycleOwner.current
+    val configuration = LocalConfiguration.current
+
+
+    val iconSize = 48.dp
 
     LaunchedEffect(videoUri) {
         viewModel.preparePlayer(videoUri)
@@ -138,9 +148,10 @@ fun VideoScreen(
                     .background(Color.Black.copy(alpha = 0.4f)) // 어두워짐
             ) {
                 Column(modifier = Modifier.systemBarsPadding()) {
-                    Row {
+                    Row(modifier = Modifier.padding(horizontal = 12.dp)) {
                         IconButton(onClick = onBackClick) {
                             Icon(
+                                modifier = Modifier.size(iconSize),
                                 imageVector = Icons.AutoMirrored.Filled.ArrowBack,
                                 contentDescription = null,
                                 tint = Color.White
@@ -148,7 +159,12 @@ fun VideoScreen(
                         }
                         Spacer(modifier = Modifier.weight(1f))
                         IconButton(onClick = { isAudioController = true }) {
-                            Icon(painterResource(R.drawable.baseline_slow_motion_video_24), null, tint = Color.White)
+                            Icon(
+                                painterResource(R.drawable.baseline_slow_motion_video_24),
+                                null,
+                                modifier = Modifier.size(iconSize),
+                                tint = Color.White
+                            )
                         }
                     }
                 }
@@ -157,29 +173,50 @@ fun VideoScreen(
     }
 
     if (isAudioController) {
-        when (windowWidthSizeClass) {
-            WindowWidthSizeClass.Compact, WindowWidthSizeClass.Medium -> {
-                ModalBottomSheet(onDismissRequest = { isAudioController = false }) {
-                    SoundCompactListView(uiState = soundUiState, updateVolume = updateVolume)
-                }
-            }
-            WindowWidthSizeClass.Expanded -> {
-                Dialog(
-                    onDismissRequest = { isAudioController = false },
-                    properties = DialogProperties(usePlatformDefaultWidth = false)
-                ) {
-                    Surface(
-                        modifier = Modifier
-                            .fillMaxWidth()
-                            .fillMaxHeight()
-                            .padding(16.dp),
-                        shape = RoundedCornerShape(16.dp),
-                        color = MaterialTheme.colorScheme.surface
+        val screenHeightDp2 = configuration.screenHeightDp.dp / 3f
+        Dialog(
+            onDismissRequest = { isAudioController = false },
+            properties = DialogProperties(usePlatformDefaultWidth = false)
+        ) {
+            Surface(
+                modifier = Modifier
+                    .padding(horizontal = 24.dp, vertical = 48.dp),
+                shape = RoundedCornerShape(24.dp),
+                color = Color.Black.copy(alpha = 0.3f)
+            ) {
+                Box(modifier = Modifier.fillMaxSize()) {
+                    LazyRow(
+                        modifier = Modifier.fillMaxSize()
+                            .padding(horizontal = 16.dp)
+                            .align(Alignment.Center),
+                        horizontalArrangement = Arrangement.spacedBy(16.dp),
+                        verticalAlignment = Alignment.CenterVertically
                     ) {
-                        SoundCompactListView(uiState = soundUiState, updateVolume = updateVolume)
+                        items(soundUiState.soundList) { sound ->
+                            SoundItem(
+                                modifier = Modifier.size(screenHeightDp2),
+                                sound = sound,
+                                circleRadius = screenHeightDp2.value.coerceAtMost(180f),
+                                updateVolume = updateVolume
+                            )
+                        }
+                    }
+                    IconButton(
+                        onClick = { isAudioController = false },
+                        modifier = Modifier
+                            .align(Alignment.TopEnd)
+                            .padding(12.dp)
+                    ) {
+                        Icon(
+                            imageVector = Icons.Default.Close, null,
+                            tint = Color.White,
+                            modifier = Modifier.size(iconSize),
+                        )
                     }
                 }
+
             }
         }
+
     }
 }
