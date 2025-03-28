79b11effd177cd3044e8906b0e24b79a8fd80a51
kim dong kyung <kdkd422@naver.com>
2025-03-28 18:58:01 +0900

    [Feat] 테마 데이터셋을 저장할 데이터 클래스

---
+++ Space_Out/app/src/main/java/com/dkproject/space_out/presentation/model/ThemeData.kt
@@ -0,0 +1,10 @@
+package com.dkproject.space_out.presentation.model
+
+import androidx.compose.ui.graphics.Color
+
+data class ThemeData(
+    val sliderEmptyColor: Color,
+    val sliderFillColor: Color,
+    val gradientStartColor: Color,
+    val gradientEndColor: Color
+)
