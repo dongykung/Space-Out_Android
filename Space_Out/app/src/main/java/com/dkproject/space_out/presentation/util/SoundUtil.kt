355362287d35abc5a7f113b084025996de86e039
kim dong kyung <kdkd422@naver.com>
2025-03-31 17:41:49 +0900

    [Feat] 사운드 유틸 데이터(가위, 심장박동)추가

--- Space_Out/app/src/main/java/com/dkproject/space_out/presentation/util/SoundUtil.kt
+++ Space_Out/app/src/main/java/com/dkproject/space_out/presentation/util/SoundUtil.kt
@@ -12,17 +12,21 @@ object SoundUtil {
     private const val BIRD = "bird"
     private const val SNOW = "snow"
     private const val KEYBOARD = "keyboard"
+    private const val Scissor = "scissor"
+    private const val Heart = "heartbeat"
 
     fun getSoundList(): List<SoundInfo> {
         return listOf(
-            SoundInfo(FIRE, soundName = R.string.fire),
-            SoundInfo(WAVE, soundName = R.string.wave),
-            SoundInfo(RAIN, soundName = R.string.rain),
-            SoundInfo(WIND, soundName = R.string.wind),
-            SoundInfo(CREEK, soundName = R.string.creek),
-            SoundInfo(BIRD, soundName = R.string.bird),
-            SoundInfo(SNOW, soundName = R.string.snow),
-            SoundInfo(KEYBOARD, soundName = R.string.keyboard),
+            SoundInfo(FIRE, soundName = R.string.fire, offImage = R.drawable.fireoff, onImage = R.drawable.fireon),
+            SoundInfo(WAVE, soundName = R.string.wave,offImage = R.drawable.waveoff, onImage = R.drawable.waveon),
+            SoundInfo(RAIN, soundName = R.string.rain,offImage = R.drawable.rainoff, onImage = R.drawable.rainon),
+            SoundInfo(WIND, soundName = R.string.wind,offImage = R.drawable.windoff, onImage = R.drawable.windon),
+            SoundInfo(CREEK, soundName = R.string.creek,offImage = R.drawable.creekoff, onImage = R.drawable.creekon),
+            SoundInfo(BIRD, soundName = R.string.bird,offImage = R.drawable.birdoff, onImage = R.drawable.birdon),
+            SoundInfo(SNOW, soundName = R.string.snow,offImage = R.drawable.snowoff, onImage = R.drawable.snowon),
+            SoundInfo(KEYBOARD, soundName = R.string.keyboard,offImage = R.drawable.keyboardoff, onImage = R.drawable.keyboardon),
+            SoundInfo(Scissor, soundName = R.string.scissor, offImage = R.drawable.scissoroff, onImage = R.drawable.scissoron),
+            SoundInfo(Heart, soundName = R.string.heart, offImage = R.drawable.heartoff, onImage = R.drawable.hearton)
         )
     }
 }
