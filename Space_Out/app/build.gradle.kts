11b8de1f96c38daf8f19fbf4fb42c0f43f4b4eaa
kim dong kyung <kdkd422@naver.com>
2025-03-31 22:28:55 +0900

    [build] media3.session

--- Space_Out/app/build.gradle.kts
+++ Space_Out/app/build.gradle.kts
@@ -85,6 +85,7 @@ dependencies {
     implementation(libs.androidx.media3.exoplayer)
     implementation(libs.androidx.media3.exoplayer.dash)
     implementation(libs.androidx.media3.ui)
+    implementation(libs.androidx.media3.session)
 
     implementation(libs.androidx.windowSize)
     implementation(libs.androidx.icons.extended)
