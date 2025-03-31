e03c261c67a7ae81116a22c33590019c053a5078
kim dong kyung <kdkd422@naver.com>
2025-03-31 17:42:12 +0900

    [Feat] SoundInfo 모델 offImage, omImage 추가

--- Space_Out/app/src/main/java/com/dkproject/space_out/presentation/model/SoundInfo.kt
+++ Space_Out/app/src/main/java/com/dkproject/space_out/presentation/model/SoundInfo.kt
@@ -1,9 +1,12 @@
 package com.dkproject.space_out.presentation.model
 
+import androidx.annotation.DrawableRes
 import androidx.annotation.StringRes
 
 data class SoundInfo(
     val fileName: String,
     val volume: Int = 0,
-    @StringRes val soundName: Int
+    @StringRes val soundName: Int,
+    @DrawableRes val offImage: Int,
+    @DrawableRes val onImage: Int,
 )
