c2db6dae4475704d4961dd74973720a5f38e7e1f
kim dong kyung <kdkd422@naver.com>
2025-03-31 17:39:38 +0900

    [Feat] 테마 색상 데이터 추가

---
+++ Space_Out/app/src/main/java/com/dkproject/space_out/presentation/theme/Color.kt
@@ -0,0 +1,64 @@
+package com.dkproject.space_out.presentation.theme
+
+import androidx.compose.ui.graphics.Color
+
+val Purple80 = Color(0xFFD0BCFF)
+val PurpleGrey80 = Color(0xFFCCC2DC)
+val Pink80 = Color(0xFFEFB8C8)
+
+val Purple40 = Color(0xFF6650a4)
+val PurpleGrey40 = Color(0xFF625b71)
+val Pink40 = Color(0xFF7D5260)
+
+val Midnight_Primary = Color(0xFFFFFFFF)
+val Midnight_EmptySlider = Color(0xFF6B7280)
+val Midnight_FillSlider = Color(0xFF38BDF9)
+val Midnight_GradientStart = Color(0xFF1F2937)
+val Midnight_GradientEnd = Color(0xFF111827)
+
+val OceanCalm_Primary = Color(0xFFFFFFFF)
+val OceanCalm_EmptySlider = Color(0xFF4B5563)
+val OceanCalm_FillSlider = Color(0XFF12B981)
+val OceanCalm_GradientStart = Color(0xFF1E40AF)
+val OceanCalm_GradientEnd = Color(0xFF1E3A8A)
+
+val CoralReef_Primary = Color(0xFFFFFFFF)
+val CoralReef_EmptySlider = Color(0xFF475569)
+val CoralReef_FillSlider = Color(0xFFFB923C)
+val CoralReef_GradientStart = Color(0xFF06B6D4)
+val CoralReef_GradientMiddle = Color(0xFF3B82F6)
+val CoralReef_GradientEnd = Color(0xFF2563EB)
+
+val Monochrome_Primary = Color(0xFFFFFFFF)
+val Monochrome_EmptySlider = Color(0xFF374151)
+val Monochrome_FillSlider = Color(0xFFE5E7EB)
+val Monochrome_GradientStart = Color(0xFF1F2937)
+val Monochrome_GradientMiddle = Color(0xFF111827)
+val Monochrome_GradientEnd = Color(0xFF030712)
+
+val Pastel_Primary = Color(0xFFFFFFFF)
+val Pastel_EmptySlider = Color(0xFF9CA3AF)
+val Pastel_FillSlider = Color(0xFFEC4899)
+val Pastel_GradientStart = Color(0xFFFBE7F3)
+val Pastel_GradientEnd = Color(0xFFD8B4FE)
+
+val Forest_Primary = Color(0xFFFFFFFF)
+val Forest_EmptySlider = Color(0xFF6B7280)
+val Forest_FillSlider = Color(0xFFFBBF24)
+val Forest_GradientStart = Color(0xFF166534)
+val Forest_GradientEnd = Color(0xFF14532D)
+
+val GalaxyNight_Primary = Color(0xFFFFFFFF)
+val GalaxyNight_EmptySlider = Color(0xFF475569)
+val GalaxyNight_FillSlider = Color(0xFFA855F7)
+val GalaxyNight_GradientStart = Color(0xFF4C1D95)
+val GalaxyNight_GradientEnd = Color(0xFF2E1065)
+
+val DeepOcean_Primary = Color(0xFFFFFFFF)
+val DeepOcean_EmptySlider = Color(0xFF374252)
+val DeepOcean_FillSlider = Color(0xFFE5E7EB)
+val DeepOcean_GradientStart = Color(0xFF0E7490)
+val DeepOcean_GradientEnd = Color(0xFF1E3A8A)
+
+val bottomSheetBackground = Color(0XFF111722)
+val subTitleColor = Color(0xFF808793)
